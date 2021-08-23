983491ff9e49d10726509150a682cec53ff9f66f
GalitAb <34810522+GalitAb@users.noreply.github.com>
2021-08-23 21:24:57 +0300

    Add files via upload

--- MM home task.py
+++ MM home task.py
@@ -28,28 +28,28 @@ class team_played(Resource):
         self.data = pd.read_csv('result_played.csv')
     def get(self,home_team): # GET request
         data_fount=self.data.loc[self.data['home_team'] == home_team].to_json(orient="records")
-        return jsonify({'Result are': data_fount})        
+        return jsonify({'data': data_fount})        
     
 class team_upcoming(Resource):    
     def __init__(self):
         self.data = pd.read_csv('result_upcoming.csv')        
     def get(self,home_team): # GET request
         data_fount=self.data.loc[self.data['home_team'] == home_team].to_json(orient="records")
-        return jsonify({'Result are': data_fount})   
+        return jsonify({'data': data_fount})   
     
 class tournament_played(Resource):
     def __init__(self):
         self.data = pd.read_csv('result_played.csv')       
     def get(self,tournament): # GET request
         data_fount=self.data.loc[self.data['tournament'] == tournament].to_json(orient="records")
-        return jsonify({'Result are': data_fount})
+        return jsonify({'data': data_fount})
     
 class tournament_upcoming(Resource):
     def __init__(self):
         self.data = pd.read_csv('result_upcoming.csv')       
     def get(self,tournament): # GET request
         data_fount=self.data.loc[self.data['tournament'] == tournament].to_json(orient="records")
-        return jsonify({'Result are': data_fount}) 
+        return jsonify({'data': data_fount}) 
     
 class AllTournaments(Resource):
     def __init__(self):
@@ -58,23 +58,24 @@ class AllTournaments(Resource):
         #self.data = pd.read_csv('result_played.csv')      
     def get(self,tournament): # GET request
         data_fount=self.csv_files.loc[self.csv_files['tournament'] == tournament].to_json(orient="records")
-        return jsonify({'Result are': data_fount})
+        return jsonify({'data': data_fount})
     
 class AllTeams(Resource):
     def __init__(self):
-        path = os.getcwd()
+        path = r'\C:\Users\galit\Desktop\api'
         self.csv_files = glob.glob(os.path.join(path, "*.csv"))
+        display(self.csvfiles)
         #self.data = pd.read_csv('result_played.csv')      
     def get(self,home_team): # GET request
         data_fount=self.csv_files.loc[self.csv_files['home_team'] == home_team].to_json(orient="records")
-        return jsonify({'Result are': data_fount})
+        return jsonify({'data': data_fount})
 
 
-api.add_resource(team_played, '/team_played/<string:home_team>')
-api.add_resource(team_upcoming, '/team_upcoming/<string:home_team>')
+api.add_resource(team_played, '/team-played/<string:home_team>')
+api.add_resource(team_upcoming, '/team-upcoming/<string:home_team>')
 api.add_resource(AllTeams, '/AllTeams/<string:home_team>')
-api.add_resource(tournament_played, '/tournament_played/<string:tournament>')
-api.add_resource(tournament_upcoming, '/tournament_upcoming/<string:tournament>')
+api.add_resource(tournament_played, '/tournament-played/<string:tournament>')
+api.add_resource(tournament_upcoming, '/tournament-upcoming/<string:tournament>')
 api.add_resource(AllTournaments, '/AllTournaments/<string:tournament>')
 
 if __name__ == '__main__':
