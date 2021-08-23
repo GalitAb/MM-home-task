5e990f38940ae9f5b7ecc7e16faa1fe693328348
GalitAb <34810522+GalitAb@users.noreply.github.com>
2021-08-23 18:09:53 +0300

    Add files via upload

--- MM home task.py
+++ MM home task.py
@@ -11,6 +11,7 @@ import pandas as pd
 app=Flask(__name__)
 api=Api(app)
 
+  
 data_arg=reqparse.RequestParser()
 data_arg.add_argument("home_team" , type=str ,help="Enter home_team")
 data_arg.add_argument("home_score" , type=int ,help="Enter home_score")
@@ -19,19 +20,23 @@ data_arg.add_argument("away_score" , type=int ,help="Enter away_score")
 data_arg.add_argument("tournament" , type=str ,help="Enter tournament")
 data_arg.add_argument("start_time" , type=str ,help="Enter start_time")
 
-class read(Resource):
-    
-    def __init__(self):        
-        self.data = pd.read_csv('result_played.csv') # reading csv file
-        
-    def get(self,hs): # GET request
-        data_fount=self.data.loc[self.data['home_score'] == hs].to_json(orient="records")
-        return jsonify({'Result': data_fount})
-        
 
-api.add_resource(read, '/<int:hs>')
-#api.add_resource(read, '/<str:home_team>')
+class findTeam(Resource):    
+    def __init__(self):
+        self.data = pd.read_csv('result_played.csv') # reading csv file        
+    def get(self,ht): # GET request
+        data_fount=self.data.loc[self.data['home_team'] == ht].to_json(orient="records")
+        return jsonify({'Result are': data_fount})        
+    
+class findTournament(Resource):
+    def __init__(self):
+        self.data = pd.read_csv('result_played.csv') # reading csv file        
+    def get(self,tournament): # GET request
+        data_fount=self.data.loc[self.data['tournament'] == tournament].to_json(orient="records")
+        return jsonify({'Result are': data_fount}) 
+
+api.add_resource(findTeam, '/findTeam/<string:ht>')
+api.add_resource(findTournament, '/findTournament/<string:tournament>')
 
 if __name__ == '__main__':
     app.run(debug=False, port=8081)
-    
